3c7db0ea40293793b9e69b8ca95b847c98e44adf
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-08-07 13:20:12 +0700

    refactor: route for cart and screen cart

--- src/navigation/MainStackNavigator.js
+++ src/navigation/MainStackNavigator.js
@@ -2,6 +2,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
+import Cart from "../screens/Cart/Cart.js";
 import AppTabNavigator from "./AppTabNavigator";
 
 const Stack = createNativeStackNavigator();
@@ -14,6 +15,7 @@ const MainStackNavigator = () => (
         component={AppTabNavigator}
         options={{ headerShown: false }}
       />
+      <Stack.Screen name="Cart" component={Cart} />
       {/* Thêm các màn hình khác không nằm trong tab ở đây */}
     </Stack.Navigator>
   </NavigationContainer>
--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -1,15 +1,27 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { ScrollView, StyleSheet } from "react-native";
+import ButtonWithIcon from "../../components/Button/ButtonIcon";
 import Categories from "../../components/Categories/Categories";
+import Row from "../../components/Layout/Row";
 import ProductBanner from "../../components/Product/ProductBanner";
 import ProductList from "../../components/Product/ProductList";
 import SearchBar from "../../components/SearchBar/SearchBar";
-// import SignIn from "../../components/sign/SignIn";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
   return (
     <ScrollView style={styles.container}>
-      <SearchBar />
+      <Row style={{ paddingHorizontal: "1%", paddingTop: "1%" }}>
+        <SearchBar />
+        <ButtonWithIcon
+          onPress={() => navigation.navigate("Cart")}
+          iconName="shopping-cart"
+          iconSize={24}
+          iconColor="tomato"
+          style={{ padding: 10, marginLeft: 10 }}
+        />
+      </Row>
       <Categories />
       <ProductBanner />
       <ProductList />
